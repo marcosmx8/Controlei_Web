508a30ba6f4705b9200b15ddf08aff8825bcfd5f
marcosmx8 <Marcosmx8@hotmail.com>
2025-04-23 00:41:43 -0300

    Publicação inicial do MVP Controlei 🚀

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Controlei - MVP de Navegação 💙</h1>
+      <p>Interface carregada com sucesso!</p>
+    </div>
+  );
+}
+
+export default App;
